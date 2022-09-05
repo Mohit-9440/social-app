2f88aedde79dcf5e1bd8da90ce0b853f785c0f8a
Mohit <mohitsharma8864@gmail.com>
2022-09-05 17:39:26 +0530

    rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -7,9 +7,24 @@ export default function Rightbar() {
             <div className="birthdayContainer">
                 <img className="birthdayImg" src="assets/gift.png" alt="" />
                 <span className="birthdayText">
-                    <b>Pola Foster</b> and <b> 3 other friends</b> have a birthday today
+                  <b>Pola Foster</b> and <b> 3 other friends</b> have a birthday today
                 </span>
             </div>
+            <img className="rightbarAd" src="/assets/ad.png" alt=""/>
+            <h4 class="rightbarTitle">Online Friends</h4>
+            <ul class="rightbarFriendList">
+              <li class="rightbarFriend">
+                <div class="rightbarProfileImgContainer">
+                  <img
+                   className="rightbarProfileImg" 
+                   src="assets/person/3.jpeg" 
+                   alt=""
+                  />
+                  <span class="rightbarOnline"></span>
+                </div>
+                <span class="rightbarUsername">Hardik</span>
+              </li>
+            </ul>
         </div>
     </div>
   )
